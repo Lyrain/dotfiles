6ca7d4f7e0bf45105489129cf087514472ded8a5
Myles Offord <mylesofford@hotmail.com>
2016-08-31 17:45:19 +0100

    Updated Emacs Config
    Following Vim to Emacs in 14 days

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,12 +1,43 @@
-;; Reqire common lisp libraries
-(require 'cl)
+;; My emacs config file
+;; Myles Offord
+(require 'package)
 
-;; Make sure we have el-get
-(unless (require 'el-get nil t)
-  (url-retrieve "https://raw.github.com/dimitri/el-get/master/el-get-install.el"
-   (lambda (s)
-     (end-of-buffer))))
-(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+;; Add a list of repositories.
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+
+;; Initialize packages
+(setq package-enable-at-startup nil)
+
+(defun fluff-ensure-package-installed (&rest packages)
+  "Assure every package is installed, ask for installation if it's not.
+
+Return a list of installed packages or nil for every skipped package."
+  (mapcar
+   (lambda (package)
+     (if (package-installed-p package)
+	 nil
+       (if (y-or-n-p (format "Package %s is missing. Install it? " package))
+	   (package-install package)
+	 package)))
+   packages))
+
+;; Make sure to have downloaded archive description.
+(or (file-exists-p package-user-dir)
+    (package-refresh-contents))
+
+;; Activeate installed packages
+(package-initialize)
+
+;; List packages for installation here.
+(fluff-ensure-package-installed 'evil
+				'evil-surround
+				'helm
+				'projectile
+				'helm-projectile
+                                'web-mode
+                                'coffee-mode)
 
 ;; Custom theme
 (add-to-list 'custom-theme-load-path "~/.emacs.d/emacs-color-theme-solarized")
@@ -14,20 +45,44 @@
 (set-frame-parameter nil 'background-mode 'dark)
 (load-theme 'solarized t)
 
-;; Remove the tool bar
-(custom-set-variables '(tool-bar-mode nil))
+;; Essential settings.
+(setq inhibit-splash-screen t
+      inhibit-startup-message t
+      inhibit-startup-echo-area-message t)
 (menu-bar-mode -1)
-(toggle-scroll-bar -1)
-
+(tool-bar-mode -1)
+(when (boundp 'scroll-bar-mode)
+  (toggle-scroll-bar -1))
+(show-paren-mode 1)
+(setq visual-line-fringe-indicators '(left-curly-arrow right-curly-arrow))
+(setq-default left-fringe-width nil)
+(setq-default indent-tabs-mode nil)
+(eval-after-load "vc" '(setq vc-handled-backends nil))
+(setq vc-follow-symlinks t)
+(setq large-file-warning-threshold nil)
+(setq split-width-threshold nil)
+(setq custom-safe-themes t)
+(put 'narrow-to-region 'disabled nil)
+(custom-set-variables
+ '(initial-frame-alist (quote ((fullscreen . maximized)))))
 ;; Change from control to command on mac
-(setq mac-command-modifier 'control)
-
-;; Allow hash to be entered
-;; (global-set-key (kbd "M-3") '(lambda()(interactive)(insert "#")))
+;; (setq mac-command-modifier 'control)
 
 ;; Evil-Mode
-;; (add-to-list 'load-path "~/.emacs.d/el-get/evil")
-;; (require 'evil)
-;; (evil-mode 1)
+(require 'evil)
+(evil-mode t)
 
+;; Evil-Surround
+(require 'evil-surround)
+(global-evil-surround-mode t)
 
+;; Helm
+(require 'helm)
+(helm-mode t)
+
+;; Projectile + Helm-projectile
+(require 'projectile)
+(projectile-mode t)
+
+(require 'helm-projectile)
+(helm-projectile-on)
