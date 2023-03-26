2d1a89d8bfd3fb80bb61f21de00742f951a6e4e3
Myles Offord <2564924+Lyrain@users.noreply.github.com>
2023-03-26 13:12:22 +0100

    polybar: launch handle monitor script

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,7 +6,8 @@ pkill polybar
 # Wait until the process have been shut down
 while pgrep polybar >/dev/null; do sleep 1; done
 
+~/.screenlayout/layout.sh && sleep 2
+
 for i in $(polybar -m | awk -F: '{print $1}'); do
-  MONITOR=$i polybar main -c ~/.config/polybar/config 2>&1 | \
-    tee /tmp/polybar-${i}.log & disown
+  MONITOR=$i polybar main -c ~/.config/polybar/config & disown
 done
